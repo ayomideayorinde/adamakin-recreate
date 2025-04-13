2d20fdaa542fd9d243d1345fd2e3d42157fe22dd
ayomideayorinde <116303702+ayomideayorinde@users.noreply.github.com>
2025-04-13 21:01:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//Menu Behavior Bar
+function menu (){
+    if (document.getElementById('links').style.display == "none") {
+        document.getElementById('links').style.display = "flex";
+    }
+    else {
+        document.getElementById('links').style.display = "none";
+    }
+    
+
+
+    
+}
